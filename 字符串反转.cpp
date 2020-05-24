b2fa0db30956e120f1219b5d5ae1ce0ccc8d7abb
gongluck <1039994845@qq.com>
2020-05-24 18:12:01 +0800

    Create 字符串反转.cpp

---
+++ 字符串反转.cpp
@@ -0,0 +1,15 @@
+// 写出一个程序，接受一个字符串，然后输出该字符串反转后的字符串。（字符串长度不超过1000）
+
+#include <iostream>
+
+int main()
+{
+    std::string str;
+    std::cin >> str;
+    for(auto it = str.rbegin(); it != str.rend(); ++it)
+    {
+        std::cout << *it;
+    }
+    
+    return 0;
+}
